6088a12182480e36e2a829a43c0bb864b71ca360
Alvaro <alvaro.delarocharamos@gmail.com>
2021-02-13 20:16:20 -0800

    Se agrego archivo de variables

---
+++ datatype.py
@@ -0,0 +1,13 @@
+#Dictionaries
+print( type({
+    "Nombre" : "Alvaro",
+    "Profesion" : "Estudiante"
+}))
+
+name = "Alvaro"
+
+print(name)
+
+book_name = "Snake case"
+bookName = "Camel case"
+BookName = "Pascal case"
